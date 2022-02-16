fabb65674255bcbeb40d1ffc16af8838a750c94e
rickyriosp <riki.rios91@gmail.com>
2022-02-16 18:51:52 -0500

    Add project files.

---
+++ BugTracker/Models/BTUserModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BugTracker.Models
+{
+    public class BTUserModel : IdentityUser
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [NotMapped]
+        [Display(Name = "Full Name")]
+        public string FullName { get => $"{FirstName} {LastName}"; }
+    }
+}
