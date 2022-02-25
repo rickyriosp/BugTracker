11ac4cbeeb007b5a8374d64f48e1a65d5c28a8ce
rickyriosp <riki.rios91@gmail.com>
2022-02-25 10:31:33 -0500

    created ticket attachment model

---
+++ BugTracker/Models/TicketAttachmentModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BugTracker.Models
+{
+    public class TicketAttachmentModel
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Ticket")]
+        public int TicketId { get; set; }
+
+        [DisplayName("File Date")]
+        public DateTimeOffset Created { get; set; }
+
+        [DisplayName("Team Member")]
+        public string UserId { get; set; }
+
+        [NotMapped]
+        [DataType(DataType.Upload)]
+        public IFormFile FormFile { get; set; }
+
+        [DisplayName("File Name")]
+        public string FileName { get; set; }
+
+        public byte[] FileData { get; set; }
+
+        [DisplayName("File Extension")]
+        public string FileContentType { get; set; }
+
+        // Navigation properties
+        public virtual Ticket Ticket { get; set; }
+        public virtual BTUserModel User { get; set; }
+    }
+}
