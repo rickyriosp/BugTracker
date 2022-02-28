7a1d2697f57c50b8ad409b26aa0b463b0e11f138
rickyriosp <riki.rios91@gmail.com>
2022-02-28 12:59:24 -0500

    created company model

---
+++ BugTracker/Models/CompanyModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+namespace BugTracker.Models
+{
+    public class CompanyModel
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Company Name")]
+        public string Name { get; set; }
+
+        [DisplayName("Company Description")]
+        public string Description { get; set; }
+
+
+        // Navigation properties
+        public virtual ICollection<BTUserModel> Members { get; set; } = new HashSet<BTUserModel>();
+        public virtual ICollection<ProjectModel> Projects { get; set; } = new HashSet<ProjectModel>();
+    }
+}
