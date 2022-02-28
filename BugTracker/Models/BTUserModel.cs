48fad98a2aa1bb20441db71a5115705fc85dc6ff
rickyriosp <riki.rios91@gmail.com>
2022-02-28 15:44:15 -0500

    updated btuser model to create project members

--- BugTracker/Models/BTUserModel.cs
+++ BugTracker/Models/BTUserModel.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -17,5 +18,26 @@ namespace BugTracker.Models
         [NotMapped]
         [Display(Name = "Full Name")]
         public string FullName { get => $"{FirstName} {LastName}"; }
+
+        [NotMapped]
+        [DataType(DataType.Upload)]
+        public IFormFile AvatarFormFile { get; set; }
+
+        [DisplayName("Avatar")]
+        public string AvatarFileName { get; set; }
+
+        [DisplayName("File Data")]
+        public byte[] AvatarFileData { get; set; }
+
+        [DisplayName("File Extension")]
+        public string AvatarContentType { get; set; }
+
+        [DisplayName("Company")]
+        public int? CompanyId { get; set; }
+
+
+        // Navigation properties
+        public virtual CompanyModel Company { get; set; }
+        public virtual ICollection<ProjectModel> Projects { get; set; } = new HashSet<ProjectModel>();
     }
 }
