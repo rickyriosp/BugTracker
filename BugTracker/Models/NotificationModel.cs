f989cfcbfe380f69a69b52af8cf6380451b9b937
rickyriosp <riki.rios91@gmail.com>
2022-02-28 16:05:50 -0500

    created notification model

---
+++ BugTracker/Models/NotificationModel.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BugTracker.Models
+{
+    public class NotificationModel
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Ticket")]
+        public int TicketId { get; set; }
+
+        [Required]
+        [DisplayName("Title")]
+        public int Title { get; set; }
+
+        [Required]
+        [DisplayName("Message")]
+        public string Message { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [DisplayName("Date")]
+        public DateTimeOffset Created { get; set; }
+
+        [Required]
+        [DisplayName("Recipient")]
+        public string RecipientId { get; set; }
+
+        [Required]
+        [DisplayName("Sender")]
+        public string SenderId { get; set; }
+
+        [DisplayName("Has been viewed")]
+        public bool Viewed { get; set; }
+
+
+        // Navigation properties
+        public virtual TicketModel Ticket { get; set; }
+        public virtual BTUserModel Recipient { get; set; }
+        public virtual BTUserModel Sender { get; set; }
+    }
+}
