61390949ba8c84b923351072bfff5046cf3b0705
rickyriosp <riki.rios91@gmail.com>
2022-03-17 19:44:25 -0400

    created IBTRolesService and scaffolded class

---
+++ BugTracker/Services/BTRolesService.cs
@@ -0,0 +1,63 @@
+﻿using BugTracker.Data;
+using BugTracker.Models;
+using BugTracker.Services.Interfaces;
+using Microsoft.AspNetCore.Identity;
+
+namespace BugTracker.Services
+{
+    public class BTRolesService : IBTRolesService
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<BTUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public BTRolesService(ApplicationDbContext context, UserManager<BTUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _context = context;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task<bool> AddUserToRoleAsync(BTUser user, string roleName)
+        {
+            bool result = (await _userManager.AddToRoleAsync(user, roleName)).Succeeded;
+
+            return result;
+        }
+
+        public Task<string> GetRoleNameByIdAsync(string roleId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<string>> GetUserRolesAsync(BTUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<BTUser>> GetUsersInRoleAsync(string roleName, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<BTUser>> GetUsersNotInRoleAsync(string roleName, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsUserEnrolledAsync(BTUser user, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveUserFromRoleAsync(BTUser user, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveUserFromRolesAsync(BTUser user, IEnumerable<string> roles)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BugTracker/Services/Interfaces/IBTRolesService.cs
@@ -0,0 +1,23 @@
+﻿using BugTracker.Models;
+
+namespace BugTracker.Services.Interfaces
+{
+    public interface IBTRolesService
+    {
+        Task<bool> IsUserEnrolledAsync(BTUser user, string roleName);
+
+        Task<IEnumerable<string>> GetUserRolesAsync(BTUser user);
+
+        Task<bool> AddUserToRoleAsync(BTUser user, string roleName);
+
+        Task<bool> RemoveUserFromRoleAsync(BTUser user, string roleName);
+
+        Task<bool> RemoveUserFromRolesAsync(BTUser user, IEnumerable<string> roles);
+
+        Task<List<BTUser>> GetUsersInRoleAsync(string roleName, int companyId);
+
+        Task<List<BTUser>> GetUsersNotInRoleAsync(string roleName, int companyId);
+
+        Task<string> GetRoleNameByIdAsync(string roleId);
+    }
+}
