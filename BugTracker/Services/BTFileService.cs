d820f667079361919566d8bb1b51ec3a1d06c2b1
rickyriosp <riki.rios91@gmail.com>
2022-04-20 15:57:03 -0400

    implemented BTFileService

---
+++ BugTracker/Services/BTFileService.cs
@@ -0,0 +1,72 @@
+﻿using BugTracker.Services.Interfaces;
+
+namespace BugTracker.Services
+{
+    public class BTFileService : IBTFileService
+    {
+        private readonly string[] suffixes = { "Bytes", "KB", "MB", "GB", "TB", "PB" };
+
+        public string ConvertByteArrayToFile(byte[] fileData, string extension)
+        {
+            try
+            {
+                string imageBase64Data = Convert.ToBase64String(fileData);
+
+                return string.Format($"data:{extension};base64,{imageBase64Data}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"***** ERROR ***** - Error Converting Byte Array to File. ---> {ex.Message}");
+                throw;
+            }
+        }
+
+        public async Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file)
+        {
+            try
+            {
+                MemoryStream memoryStream = new();
+
+                await file.CopyToAsync(memoryStream);
+                byte[] fileData = memoryStream.ToArray();
+
+                memoryStream.Close();
+                memoryStream.Dispose();
+
+                return fileData;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"***** ERROR ***** - Error Accepting Invite. ---> {ex.Message}");
+                throw;
+            }
+        }
+
+        public string FormatFileSize(long bytes)
+        {
+            int counter = 0;
+            decimal fileSize = bytes;
+
+            while (Math.Round(fileSize / 1024) > 0)
+            {
+                fileSize /= 1024;
+                counter++;
+            }
+
+            return string.Format("{0:n1}{1}", fileSize, suffixes[counter]);
+        }
+
+        public string GetFileIcon(string file)
+        {
+            string fileImage = "default";
+
+            if (!string.IsNullOrWhiteSpace(file))
+            {
+                fileImage = Path.GetExtension(file).Replace(".", "");
+                return $"/img/png/{fileImage}.png";
+            }
+
+            return fileImage;
+        }
+    }
+}
---
+++ BugTracker/Services/Interfaces/IBTFileService.cs
@@ -0,0 +1,13 @@
+﻿namespace BugTracker.Services.Interfaces
+{
+    public interface IBTFileService
+    {
+        string ConvertByteArrayToFile(byte[] fileData, string extension);
+
+        Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file);
+
+        string FormatFileSize(long bytes);
+
+        string GetFileIcon(string file);
+    }
+}
