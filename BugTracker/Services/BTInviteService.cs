d7a3ab366f46e9f26ab0a6f1f9753c5a535d217e
rickyriosp <riki.rios91@gmail.com>
2022-04-20 14:44:48 -0400

    implemented BTInviteService

---
+++ BugTracker/Services/BTInviteService.cs
@@ -0,0 +1,154 @@
+﻿using BugTracker.Data;
+using BugTracker.Models;
+using BugTracker.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace BugTracker.Services
+{
+    public class BTInviteService : IBTInviteService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BTInviteService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> AcceptInviteAsync(Guid? token, string userId, int companyId)
+        {
+            Invite invite = await _context.Invites.FirstOrDefaultAsync(i => i.CompanyToken == token);
+
+            if (invite == null)
+            {
+                return false;
+            }
+
+            try
+            {
+                invite.IsValid = false;
+                invite.InviteeId = userId;
+                await _context.SaveChangesAsync();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"***** ERROR ***** - Error Accepting Invite. ---> {ex.Message}");
+                throw;
+            }
+        }
+
+        public async Task AddNewInviteAsync(Invite invite)
+        {
+            try
+            {
+                await _context.Invites.AddAsync(invite);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"***** ERROR ***** - Error Adding New Invite. ---> {ex.Message}");
+                throw;
+            }
+        }
+
+        public async Task<bool> AnyInviteAsync(Guid token, string email, int companyId)
+        {
+            try
+            {
+                bool result = await _context.Invites
+                    .Where(i => i.CompanyId == companyId)
+                    .AnyAsync(i => i.CompanyToken == token && i.InviteeEmail == email);
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"***** ERROR ***** - Error Checking Invite. ---> {ex.Message}");
+                throw;
+            }
+        }
+
+        public async Task<Invite> GetInviteAsync(int inviteId, int companyId)
+        {
+            try
+            {
+                Invite invite = await _context.Invites
+                    .Where(i => i.CompanyId == companyId)
+                    .Include(i => i.Company)
+                    .Include(i => i.Project)
+                    .Include(i => i.Invitor)
+                    .AsSplitQuery()
+                    .FirstOrDefaultAsync(i => i.Id == inviteId);
+
+                return invite;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"***** ERROR ***** - Error Retrieving Invite. ---> {ex.Message}");
+                throw;
+            }
+        }
+
+        public async Task<Invite> GetInviteAsync(Guid token, string email, int companyId)
+        {
+            try
+            {
+                Invite invite = await _context.Invites
+                    .Where(i => i.CompanyId == companyId)
+                    .Include(i => i.Company)
+                    .Include(i => i.Project)
+                    .Include(i => i.Invitor)
+                    .AsSplitQuery()
+                    .FirstOrDefaultAsync(i => i.CompanyToken == token && i.InviteeEmail == email);
+
+                return invite;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"***** ERROR ***** - Error Retrieving Invite. ---> {ex.Message}");
+                throw;
+            }
+        }
+
+        public async Task<bool> ValidateInviteCodeAsync(Guid? token)
+        {
+            if (token == null)
+            {
+                return false;
+            }
+
+            try
+            {
+                bool result = false;
+                
+                Invite invite = await _context.Invites.FirstOrDefaultAsync(i => i.CompanyToken == token);
+
+                if (invite == null)
+                {
+                    return false;
+                }
+
+                // Determine invite date
+                DateTime inviteDate = invite.InviteDate.DateTime;
+
+                // Custom validation of invite based on the date it was issued
+                // In this case we are allowing an invite to be valid for 7 days
+                bool validDate = DateTime.Now.Subtract(inviteDate).TotalDays <= 7;
+
+                if (validDate)
+                {
+                    // Invite is invalidated once it's accepted in the Accept method, otherwise still good
+                    result = invite.IsValid;
+                }
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"***** ERROR ***** - Error Validating Invite. ---> {ex.Message}");
+                throw;
+            }
+        }
+    }
+}
--- BugTracker/Services/Interfaces/IBTNotificationService.cs
+++ BugTracker/Services/Interfaces/IBTNotificationService.cs
@@ -4,16 +4,16 @@ namespace BugTracker.Services.Interfaces
 {
     public interface IBTNotificationService
     {
-        public Task AddNotificationAsync(Notification notification);
+        Task AddNotificationAsync(Notification notification);
 
-        public Task<List<Notification>> GetReceivedNotificationsAsync(string userId);
+        Task<List<Notification>> GetReceivedNotificationsAsync(string userId);
 
-        public Task<List<Notification>> GetSentNotificationsAsync(string userId);
+        Task<List<Notification>> GetSentNotificationsAsync(string userId);
 
-        public Task SendEmailNotificationsByRoleAsync(Notification notification, int companyId, string role);
+        Task SendEmailNotificationsByRoleAsync(Notification notification, int companyId, string role);
 
-        public Task SendMembersEmailNotificationsAsync(Notification notification, List<BTUser> members);
+        Task SendMembersEmailNotificationsAsync(Notification notification, List<BTUser> members);
 
-        public Task<bool> SendEmailNotificationAsync(Notification notification, string emailSubject);
+        Task<bool> SendEmailNotificationAsync(Notification notification, string emailSubject);
     }
 }
