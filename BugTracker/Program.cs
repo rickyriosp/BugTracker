b718684eac5067730e52c04e33a6092de801dd48
rickyriosp <riki.rios91@gmail.com>
2022-04-21 17:48:02 -0400

    implemented DataUtility class

--- BugTracker/Program.cs
+++ BugTracker/Program.cs
@@ -10,7 +10,9 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var connectionString = DataUtility.GetConnectionString(builder.Configuration);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
@@ -71,5 +73,7 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
+
+await DataUtility.ManageDataAsync(app);
 
 app.Run();
