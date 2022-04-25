f8aa47a3a14f94495512d20dc90e244733014930
rickyriosp <riki.rios91@gmail.com>
2022-04-25 14:06:20 -0400

    implemented manage user roles POST action

--- BugTracker/Controllers/UserRolesController.cs
+++ BugTracker/Controllers/UserRolesController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BugTracker.Controllers
 {
+    [Authorize]
     public class UserRolesController : Controller
     {
         private readonly IBTRolesService _rolesService;
@@ -24,10 +25,9 @@ namespace BugTracker.Controllers
         [Authorize(Roles = ("Admin"))]
         public async Task<IActionResult> ManageUserRoles()
         {
-            List<ManageUserRolesViewModel> model = new();
-
             int companyId = User.Identity.GetCompanyId().Value;
 
+            List<ManageUserRolesViewModel> model = new();
             List<BTUser> users = await _companyInfoService.GetAllMembersAsync(companyId);
 
             foreach (var user in users)
@@ -41,6 +41,29 @@ namespace BugTracker.Controllers
             }
 
             return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ManageUserRoles(ManageUserRolesViewModel member)
+        {
+            int companyId = User.Identity.GetCompanyId().Value;
+
+            BTUser btUser = (await _companyInfoService.GetAllMembersAsync(companyId)).FirstOrDefault(u => u.Id == member.BTUser.Id);
+            IEnumerable<string> roles = await _rolesService.GetUserRolesAsync(btUser);
+            
+            // For multiple roles do a foreach and assign all selected roles
+            string userRole = member.SelectedRoles.FirstOrDefault();
+
+            if (!string.IsNullOrEmpty(userRole))
+            {
+                if (await _rolesService.RemoveUserFromRolesAsync(btUser, roles))
+                {
+                    await _rolesService.AddUserToRoleAsync(btUser, userRole);
+                }
+            }
+
+            return RedirectToAction(nameof(ManageUserRoles));
         }
     }
 }
