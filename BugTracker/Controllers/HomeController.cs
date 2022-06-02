462ce3ff7181a1414784772ba876d725a6241a6f
rickyriosp <riki.rios91@gmail.com>
2022-06-02 09:02:00 -0400

    dashboard get method

--- BugTracker/Controllers/HomeController.cs
+++ BugTracker/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using BugTracker.Models;
+﻿using BugTracker.Extensions;
+using BugTracker.Models;
+using BugTracker.Models.ViewModels;
+using BugTracker.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,15 +10,30 @@ namespace BugTracker.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IBTCompanyInfoService _companyInfoService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IBTCompanyInfoService companyInfoService)
         {
             _logger = logger;
+            _companyInfoService = companyInfoService;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> Dashboard()
+        {
+            int companyId = User.Identity.GetCompanyId().Value;
+
+            DashboardViewModel model = new();
+            model.Company = await _companyInfoService.GetCompanyInfoByIdAsync(companyId);
+            model.Projects = (await _companyInfoService.GetAllProjectsAsync(companyId)).Where(p => p.Archived == false).ToList();
+            model.Tickets = model.Projects.SelectMany(p => p.Tickets).Where(t => t.Archived == false).ToList();
+            model.Members = model.Company.Members.ToList();
+
+            return View(model);
         }
 
         public IActionResult Privacy()
